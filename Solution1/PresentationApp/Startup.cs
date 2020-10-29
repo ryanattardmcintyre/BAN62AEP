c0b2aaed1e2e9afa1245d2f698b6c6ddc5d534f6
ryan.attard <ryan.attard@mcast.edu.mt>
2020-10-29 09:18:08 +0100

    commit before migration

--- Solution1/PresentationApp/Startup.cs
+++ Solution1/PresentationApp/Startup.cs
@@ -12,6 +12,7 @@ using PresentationApp.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ShoppingCart.Data.Context;
 
 namespace PresentationApp
 {
@@ -27,11 +28,18 @@ namespace PresentationApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer(
+            Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDbContext<ShoppingCartDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
+          
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+           
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
---
+++ Solution1/ShoppingCart.Domain/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace ShoppingCart.Domain.Models
+{
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+      
+        [Required]
+        public double Price { get; set; }
+
+        public int Stock { get; set; }
+
+        [Required]
+        public Category Category { get; set; }
+
+         
+    }
+}
